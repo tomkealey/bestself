782c4e24d4b6803a413269d51b8d92d908332949
tomkealey <tom.kealey@qlik.com>
2022-01-01 17:43:32 +0100

    refactor: add routes

--- src/App.js
+++ src/App.js
@@ -1,87 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { getRandomNumber, getContrastYIQ } from './utils/Helper';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  const [category, setCategory] = useState('Press the spacebar');
-  const [show, setShow] = useState(true);
-  const [colour, setColour] = useState();
-  const [contrast, setContrast] = useState();
-
-  const cards = {
-    categories: ['Life', 'Random','Deep', 'Experiences', 'If you could...', 'Would you rather...'],
-    colours: ['#E0E0E0', '#F5F500', '#FF1F1F', '#009DFF', '#21A300', '#6F5F81']
-  }
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  });
-
-  useEffect(() => {
-    document.addEventListener('touchstart', handleTocuhStart);
-    return () => {
-      document.removeEventListener('touchstart', handleTocuhStart);
-    };
-  });
-
-  function handleKeyDown(event) {
-    switch(event.code) {
-      case 'Space':
-        // pick a random card
-        pickCard(getRandomNumber());
-        break;
-      case 'KeyL': case 'Digit0':
-        // Life  
-        pickCard(0);
-        break;
-      case 'KeyR': case 'Digit1':
-        // Random  
-        pickCard(1);
-        break;
-      case 'KeyD': case 'Digit2':
-        // Deep  
-        pickCard(2);
-        break;
-      case 'KeyE': case 'Digit3':
-        // Experience  
-        pickCard(3);
-        break;
-      case 'KeyI': case 'Digit4':
-        // If you could...
-        pickCard(4);
-        break;
-      case 'KeyW': case 'Digit5':  
-        // Would you rather...
-        pickCard(5);
-        break;
-      default:
-        // do nothing
-    }
-  }
-
-  function handleTocuhStart(event) {
-    pickCard(getRandomNumber()); 
-  }
-
-  // pick a card 
-  const pickCard = (x) => {
-    setShow(false);
-    setCategory(cards.categories[x]);
-    setColour(cards.colours[x]);
-    //contrast text colour against card color background
-    setContrast(getContrastYIQ(cards.colours[x]));
-  }
-
   return (
     <div className="App">
-      <header className="App-header" style={{ backgroundColor: `${colour}`, color: `${contrast}` }}>
-        <p>{category}</p>
-        <p className="App-cards">{show && cards.categories.join(' | ')}</p>
+      <header className="App-header">
+        <NavLink to="/icebreaker">Icebreaker</NavLink>
       </header>
-      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import App from './App';
+import Icebreaker from './routes/Icebreaker';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+
+render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/icebreaker" element={<Icebreaker />} />
+      <Route 
+        path="*" 
+        element={
+          <div className="App">
+            <header className="App-header">
+              <p>There's nothing here!</p>
+            </header>
+          </div>
+        } 
+      />
+    </Routes>
+  </Router>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
