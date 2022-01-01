91c173ae39fb011ef81f27899e704cc8acfd6800
tomkealey <tom.kealey@qlik.com>
2022-01-02 00:32:54 +0100

    refactor: tidy up App list

--- src/routes/App.js
+++ src/routes/App.js
@@ -9,51 +9,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <ButtonGroup
-        orientation="vertical"
-        aria-label="vertical contained button group"
-        variant="text"
-      >
-        <IconButton 
-          component={Link} to={'/courage'} 
-          color="inherit"
-          edge="start"
-        >
+      <ButtonGroup orientation="vertical">
+        <IconButton component={Link} to={'/courage'} color="inherit">
           courage<ArrowForwardIcon />
         </IconButton>
-        <IconButton 
-          component={Link} to={'/edison'} 
-          color="inherit"
-          edge="start"
-        >
+        <IconButton component={Link} to={'/edison'} color="inherit">
           edison<ArrowForwardIcon />
         </IconButton>
-        <IconButton 
-          component={Link} to={'/icebreaker'} 
-          color="inherit"
-          edge="start"
-        >
+        <IconButton component={Link} to={'/icebreaker'} color="inherit">
           icebreaker<ArrowForwardIcon />
         </IconButton>
-        <IconButton 
-          component={Link} to={'/intimacy'} 
-          color="inherit"
-          edge="start"
-        >
+        <IconButton component={Link} to={'/intimacy'} color="inherit">
           intimacy<ArrowForwardIcon />
         </IconButton>
-        <IconButton 
-          component={Link} to={'/littletalk'} 
-          color="inherit"
-          edge="start"
-        >
+        <IconButton component={Link} to={'/littletalk'} color="inherit">
           littletalk<ArrowForwardIcon />
         </IconButton>
-        <IconButton 
-          component={Link} to={'/words'} 
-          color="inherit"
-          edge="start"
-        >
+        <IconButton component={Link} to={'/words'} color="inherit">
           words<ArrowForwardIcon />
         </IconButton>
       </ButtonGroup>
